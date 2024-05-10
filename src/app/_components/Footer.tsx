4030d0535a9fb683c452a6a0dd97b2ba3c1b393c
Tushar Inani <tusharinani93@gmail.com>
2024-05-10 19:32:37 +0000

    added: footer

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='place-middle f-col gap-8' style={{ padding: 16 }}>
+      <p className='c0'>
+        I'm dedicated to helping entrepreneurs and CEOs like you achieve your
+        dreams and ambitions
+      </p>
+      <p className='c0'>
+        Hire me, and together we can turn your vision into reality
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
